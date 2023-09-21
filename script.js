e47f95788c5251cbda8631c8de58c9614966f3f6
Adelekun Adetomiwa Kolapo <adetomiwaadelekun@gmail.com>
2023-09-21 16:10:02 +0100

    changes

--- script.js
+++ script.js
@@ -3,11 +3,6 @@
 const share = document.querySelector(".share");
 const hidden = document.querySelector(".hidden");
 share.addEventListener("click", function () {
-  console.log("You clicked");
-
-  share.style.backgroundColor = "hsl(217, 19%, 35%)";
-  // document.querySelector(".share").classList.toggle("hidden");
-  // share.classList.toggle("color");
   hidden.classList.toggle("hidden");
   document.querySelector(".container").style.marginTop = "2rem";
 });
